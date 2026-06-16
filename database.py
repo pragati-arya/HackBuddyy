7b03fa941c54459141a0fbe8bf24ef3984704ae3
pragati-arya <pragati2504arya@gmail.com>
2026-06-17 01:35:33 +0530

    Switch to PostgreSQL

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./students.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
